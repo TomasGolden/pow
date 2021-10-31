327bc6a1694eefaedbb01ad75acfead4dbd9d585
TomasGolden <tomygoldenhorn@hotmail.com>
2021-10-30 21:39:49 -0300

    Test finalizado

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     let prices = [7421, 7421];
-    let inputs = document.getElementsByClassName('cant');
+    let inputs = document.querySelectorAll('input');
     let minus = document.getElementsByClassName('minus');
     let plus = document.getElementsByClassName('plus');
     
-    for(let i=0; i < plus.length; i++)
+    for(let i=0; i < prices.length; i++)
     {
         minus[i].onclick = () => {
             if(inputs[i].value > 0) {
@@ -14,23 +14,31 @@ window.onload = function() {
         }
         
         plus[i].onclick = () => {
-            if(inputs[i].value < 15) {
+            if(inputs[i].value < 9) {
                 inputs[i].value++; 
                 recalculate(prices);
             } 
-        } 
+        }
+        inputs[i].onchange = () => {
+            recalculate(prices);
+        }
+
     }
     
 }
 
 function recalculate(prices) {
-    let acum = 0
-    let inputs = document.getElementsByClassName('cant');
+    let acum = 0;
+    let qtyacum = 0;
+    let qty = document.getElementsByClassName('cant');
+    let counter = document.getElementById('counter');
     let subtotal = document.getElementById('subtotal');
     let total = document.getElementById('total');
     for(let i = 0; i < prices.length; i++) {
-        acum = acum + prices[i] * inputs[i].value;
+        acum = acum + prices[i] * qty[i].value;
+        qtyacum = qtyacum + Number(qty[i].value);
     }
-    subtotal.value = acum;
-    total.value = acum;
+    subtotal.innerHTML = acum;
+    total.innerHTML = acum;
+    counter.innerText = qtyacum;
 }
